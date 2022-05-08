e801283585dc2e4b532f05b7a193e7e6b284b92e
aliaksandrkochneu <madjok87@icloud.com>
2022-05-08 11:38:37 +0300

    feat:keyboard was maked

---
+++ src/scripts/description.js
@@ -0,0 +1,24 @@
+function createDescription () {
+    const body = document.querySelector('body');
+    const paragraph = document.createElement('p');
+    const language = document.createElement('p');
+    paragraph.className = 'description';
+    language.className = 'language';
+    paragraph.innerText = 'Клавиатура создана в операционной системе Apple';
+    language.innerText = 'Для переключения языка комбинация: левыe ctrl + alt'
+    body.append(paragraph, language);
+  }
+
+  function createTextArea () {
+    const body = document.querySelector('body')
+    const textarea = document.createElement('textarea');
+    textarea.className = 'textarea';
+    textarea.id = 'textarea';
+    textarea.rows = '5';
+    textarea.cols = '50';
+
+    body.appendChild(textarea)
+  }
+
+// createTextArea();
+// createDescription();
---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+import '../assets/styles/base.scss'
+import './keyboard'
+
+import './description';
+
+
+
